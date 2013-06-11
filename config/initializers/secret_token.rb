7e290438f3eff7f0b2b58a5d4001ff00330e4f28
Nick Gottlieb <ngottlieb@gmail.com>
2013-06-11 10:40:07 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knotweed::Application.config.secret_token = '68fff9e80698df71eb747c7e092e8e8573faf927956cdc1c4ee0f7fa4b9b882437545c6c25518f614c9c659e0c30fcb6c49fe0d90e948270fffef68d7d4cc1b2'
