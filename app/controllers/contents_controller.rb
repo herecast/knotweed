687279dc31543ec6622ec05926f9a420adeba845
Nick Gottlieb <ngottlieb@gmail.com>
2013-08-18 21:17:37 +0000

    initial kim access added to contents index

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,6 +1,28 @@
+require "java"
+$CLASSPATH << "/home/mpatek/projects/kim/kim-stack/kim-platform-3.7/kim-api.jar"
+$CLASSPATH << "/home/mpatek/projects/kim/kim-stack/kim-platform-3.7/lib/.*jar"
+
+java_import java.rmi.RemoteException
+java_import com.ontotext.kim.client.GetService
+java_import com.ontotext.kim.client.KIMService
+java_import com.ontotext.kim.client.corpora.CorporaAPI
+java_import com.ontotext.kim.client.documentrepository.DocumentRepositoryAPI
+java_import com.ontotext.kim.client.semanticannotation.SemanticAnnotationAPI
+java_import com.ontotext.kim.client.query.QueryAPI
+java_import com.ontotext.kim.client.coredb.CoreAPI
+
 class ContentsController < ApplicationController
   def index
     @contents = Content.all
+
+    begin
+      KIMService serviceKim = GetService.from()
+      @version = serviceKim.getVersion()
+    rescue StandardError => err
+      print "Error connecting to KIM: " + err
+    end
+      
+
   end
 
   def show
