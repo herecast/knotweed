597bc6aac82a55f7455b78ee4ef6e8ac2340d8f6
Nick Gottlieb <ngottlieb@gmail.com>
2013-09-04 12:26:08 -0400

    channel matching to categories

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -6,13 +6,23 @@ class Channel < ActiveRecord::Base
 
   # gets corresponding channel for a piece of KIM content
   # (not currently mapped to our model Content)
-  def self.get_channel_for_content(content)
-    if content.features.has_key? "section"
-      c = Channel.where("categories LIKE \"%#{content.features["section"].downcase}\\n%\"").first 
-    else
-      c = Channel.find_by_name("News") or Channel.first
+  def self.get_channels_for_content(content)
+    channels = []
+    
+    if content.features.has_key? "CATEGORIES"
+      cats = content.features["CATEGORIES"].split(',').map { |c| c.strip }
+      cats.each do |category|
+        chan = Channel.where("categories LIKE \"%#{category}\\n%\"").first
+        unless chan.nil?
+          channels << chan
+        end
+      end
     end
-    return c.name.downcase
+    if channels.empty?
+      channels << Channel.find_by_name("News") unless Channel.find_by_name("News").nil?
+    end
+    
+    return channels
   end
 
 end
