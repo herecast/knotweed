a06ceefdb3f556ef87bfb987373dd8c823737a68
Nick Gottlieb <ngottlieb@gmail.com>
2013-09-25 12:46:33 -0400

    test for adding import jobs to delayed job queue on create

--- spec/models/import_job_spec.rb
+++ spec/models/import_job_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe ImportJob do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "after create" do
+    
+    it "should schedule a delayed job" do
+      job_count = Delayed::Job.count
+      job = FactoryGirl.create(:import_job)
+      Delayed::Job.count.should== job_count + 1
+    end
+    
+  end
+
 end
