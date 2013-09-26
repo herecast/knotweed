5ab912d01305970ec98a478335c8449a8fc1518f
Nick Gottlieb <ngottlieb@gmail.com>
2013-09-26 12:24:12 -0400

    removed controller after_create test for creating delayed job

--- spec/models/import_job_spec.rb
+++ spec/models/import_job_spec.rb
@@ -4,12 +4,6 @@ describe ImportJob do
 
   describe "after create" do
     
-    it "should schedule a delayed job" do
-      job_count = Delayed::Job.count
-      job = FactoryGirl.create(:import_job)
-      Delayed::Job.count.should== job_count + 1
-    end
-    
   end
 
 end
