979364835907247c9cd0bd1f21e66d7c78ee3c08
Nick Gottlieb <ngottlieb@gmail.com>
2014-01-16 12:21:27 +0000

    added address field to contact model and form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ActiveRecord::Base
-  attr_accessible :email, :name, :notes, :phone, :contact_type, :publication_ids, :organization_ids
+  attr_accessible :email, :name, :notes, :phone, :contact_type, :publication_ids,
+                  :organization_ids, :address
 
   has_and_belongs_to_many :publications
   has_and_belongs_to_many :organizations
