88eb9d89518b2ee196b23de5ebafe49119d4c783
Nick Gottlieb <ngottlieb@gmail.com>
2014-01-23 02:13:07 +0000

    fixed bug where dates werent persisting in contents search fields

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -1,2 +1,13 @@
 module ContentsHelper
+
+  def date_search_field_value(key)
+    if session[:contents_search].present?
+      session[:contents_search][key]
+    elsif params[:q].present?
+      params[:q][key]
+    else
+      nil
+    end
+  end
+
 end
