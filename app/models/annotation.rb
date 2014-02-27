76ecf667c2d0cdbdb26d1a51f046acb00ae628f4
Mike Patek <mpatek@gmail.com>
2014-02-27 12:50:46 -0500

    Adding edges routine to annotation model.

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Annotation < ActiveRecord::Base
 
   belongs_to :annotation_report
@@ -13,4 +15,44 @@ class Annotation < ActiveRecord::Base
       "pending"
     end
   end
+
+  def edges
+    
+    edges = nil
+
+    if lookup_class
+      query = CGI::escape "
+      PREFIX sbtxo:<http://www.subtext.org/ontology/>
+      PREFIX sbtxr:<http://www.subtext.org/resource/>
+      PREFIX sbtxd: <http://www.subtext.org/Document/>
+      PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX pub: <http://ontology.ontotext.com/publishing#>
+
+      SELECT * 
+      WHERE { 
+        <#{lookup_class}>  ?predicate ?object .
+
+        FILTER (
+          ?predicate != <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> 
+          )
+
+      }"
+
+      options = { :headers => { "Accept" => "application/sparql-results+json" } }
+
+      response = HTTParty.get(Figaro.env.SESAME_RDF_ENDPOINT + "/repositories/subtext?query=#{query}&queryLn=sparql", options)
+
+      if response.code == 200
+        response.body
+        result = JSON.parse(response.body)
+        edges = result["results"]["bindings"]
+      end
+
+    end
+
+    edges
+
+  end
+
 end
--- spec/models/annotation_spec.rb
+++ spec/models/annotation_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pp'
 
 describe Annotation do
   describe "status" do
@@ -14,6 +15,26 @@ describe Annotation do
       ann = FactoryGirl.create(:annotation, accepted: false)
       ann.status.should== "rejected"
     end
+  end
+
+  describe "edges" do
+    it "should return nil when it has no lookup_class" do
+      ann = FactoryGirl.create(:annotation, lookup_class: nil)
+      ann.edges.should== nil
+    end
+    it "should return an nil when lookup class is bad" do
+      ann = FactoryGirl.create(:annotation, lookup_class: "bad_lookup_class")
+      ann.edges.should== nil
+    end
+    it "should return an empty list when lookup class is not found" do
+      ann = FactoryGirl.create(:annotation, lookup_class: "http://www.subtext.org/resource/Company_T.12345")
+      ann.edges.should== []
+    end
+    it "should return an non-empty list when lookup class is found" do
+      ann = FactoryGirl.create(:annotation, lookup_class: "http://www.subtext.org/resource/Company_T.7685")
+      ann.edges.length.should be >= 1
+    end
+
   end
       
 end
