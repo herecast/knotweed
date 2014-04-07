5c9292d23990cf3a56630976215e198d2059f1ce
Nick Gottlieb <ngottlieb@gmail.com>
2014-04-07 20:50:58 +0000

    adjusted test parsing stuff to be within paresrs submodule

--- lib/test_parsers/parser_that_errors.rb
+++
@@ -1,7 +0,0 @@
-# parser_that_errors.rb
-#
-# a parser for testing that throws an error
-
-def parse_file(source, config)
-  raise StandardError, "fake error for testing"
-end
--- lib/test_parsers/parser_that_outputs_config.rb
+++
@@ -1,9 +0,0 @@
-# this parser is used by test suite to confirm that import 
-# jobs are functioning properly
-# must have timestamp and guid as config
-
-def parse_file(path, config)
-  output = []
-  output << config
-  return output.to_json
-end
