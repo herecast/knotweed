0919930ff2f215f091ba4877c51eefd5bb512ad9
Nick Gottlieb <ngottlieb@gmail.com>
2014-04-07 15:26:08 +0000

    added published filter

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -1,7 +1,9 @@
 module ContentsHelper
 
-  def date_search_field_value(key)
-    if session[:contents_search].present?
+  def search_field_value(key)
+    if params[:reset]
+      nil
+    elsif session[:contents_search].present?
       session[:contents_search][key]
     elsif params[:q].present?
       params[:q][key]
