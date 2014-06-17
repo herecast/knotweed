f8db063689c3c611ef2028a8d871ca1a59adabf7
Nick Gottlieb <ngottlieb@gmail.com>
2014-06-17 18:26:00 +0000

    fixed error with fixnum string interpolation in parent_id xml

--- app/models/content.rb
+++ app/models/content.rb
@@ -319,7 +319,7 @@ class Content < ActiveRecord::Base
                     key, value = "LOCATION", import_location.city
                   end
                 elsif k == "parent_id" and parent.present?
-                  key, value = "PARENT", Figaro.env.document_prefix + id
+                  key, value = "PARENT", "#{Figaro.env.document_prefix}#{id}"
                 end
               else
                 key = k.upcase
