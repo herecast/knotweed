61112ec37d748febdaf7ddd870a190e425f46d4d
Ben Peters <ben@bencpeters.com>
2014-09-16 10:23:09 +0000

    Added content_category model

---
+++ app/models/content_category.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: content_categories
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class ContentCategory < ActiveRecord::Base
+  attr_accessible :name
+
+  has_many :content
+
+  CATEGORIES = %w(beta_talk business campaign discussion event for_free lifestyle 
+                  local_news nation_world offered presentation recommendation
+                  sale_event sports wanted)
+end
