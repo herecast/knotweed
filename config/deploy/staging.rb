dedb56aa3e78711b9d6c43b5a5ed6eb39aa7b475
Ben Peters <ben@bencpeters.com>
2014-10-07 18:37:49 +0000

    Changed staging deploy target to point to staging.subtext.org and created production target

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :stage, :staging
 set :rails_env, 'production'
 set :deploy_to, "/var/www/knotweed-admin"
-server 'localhost', roles: %w{web app db}, primary: true, user: 'deploy'
+server 'staging.subtext.org', roles: %w{web app db}, primary: true, user: 'deploy'
 
 set :restart_delayed_jobs, true
