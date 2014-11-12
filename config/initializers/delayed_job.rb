5a6a4e1df84e2c5e0d12dadbfc745644d1754bf2
Nick Gottlieb <ngottlieb@gmail.com>
2014-11-12 16:41:26 +0000

    increase max attempts for delayed jobs to 5

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,4 +1,4 @@
 Delayed::Worker.backend = :active_record
-Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_attempts = 5
 Delayed::Worker.max_run_time = 1.month
 # Delayed::Worker.delay_jobs = !Rails.env.test?
