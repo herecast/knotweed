4103f28f59b81f80c74a5207c113326b45a31b18
Nick Gottlieb <ngottlieb@gmail.com>
2014-11-20 21:03:28 +0000

    updated geocoding logic to use business_location.name (as well as addr) IF locate_include_name

--- app/models/business_location.rb
+++ app/models/business_location.rb
@@ -24,12 +24,21 @@ class BusinessLocation < ActiveRecord::Base
   attr_accessible :address, :email, :hours, :name, :publication_id, :phone, 
     :latitude, :longitude, :venue_url, :locate_include_name
 
-  geocoded_by :address
+  geocoded_by :geocoding_address
 
-  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+  after_validation :geocode, if: ->(obj){ obj.address.present? and (obj.address_changed? or obj.name_changed? or obj.locate_include_name_changed?)}
 
   def select_option_label
     "#{name} - #{address}"
+  end
+
+  def geocoding_address
+    addr = ""
+    if locate_include_name
+      addr += name + " "
+    end
+    addr += address
+    addr
   end
 
 end
