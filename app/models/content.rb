271e767e67bfb70d0b2e0b8d4e8ed8d076b14a7c
Nick Gottlieb <ngottlieb@gmail.com>
2015-02-17 17:23:45 -0500

    removed unnecessary fields from content model (and corresponding minor code changes)

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,54 +3,35 @@
 #
 # Table name: contents
 #
-#  id                   :integer          not null, primary key
-#  title                :string(255)
-#  subtitle             :string(255)
-#  authors              :string(255)
-#  raw_content          :text
-#  issue_id             :integer
-#  import_location_id   :integer
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
-#  copyright            :string(255)
-#  guid                 :string(255)
-#  pubdate              :datetime
-#  source_category      :string(255)
-#  topics               :string(255)
-#  summary              :text
-#  url                  :string(255)
-#  origin               :string(255)
-#  mimetype             :string(255)
-#  language             :string(255)
-#  page                 :string(255)
-#  wordcount            :string(255)
-#  authoremail          :string(255)
-#  source_id            :integer
-#  file                 :string(255)
-#  quarantine           :boolean          default(FALSE)
-#  doctype              :string(255)
-#  timestamp            :datetime
-#  contentsource        :string(255)
-#  import_record_id     :integer
-#  source_content_id    :string(255)
-#  parent_id            :integer
-#  event_type           :string(255)
-#  start_date           :datetime
-#  end_date             :datetime
-#  cost                 :string(255)
-#  recurrence           :string(255)
-#  links                :text
-#  host_organization    :string(255)
-#  business_location_id :integer
-#  featured             :boolean          default(FALSE)
-#  content_category_id  :integer
-#  category_reviewed    :boolean          default(FALSE)
-#  processed_content    :text
-#  event_title          :string(255)
-#  event_description    :text
-#  event_url            :string(255)
-#  sponsor_url          :string(255)
-#  has_event_calendar   :boolean          default(FALSE)
+#  id                  :integer          not null, primary key
+#  title               :string(255)
+#  subtitle            :string(255)
+#  authors             :string(255)
+#  raw_content         :text
+#  issue_id            :integer
+#  import_location_id  :integer
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  copyright           :string(255)
+#  guid                :string(255)
+#  pubdate             :datetime
+#  source_category     :string(255)
+#  topics              :string(255)
+#  summary             :text
+#  url                 :string(255)
+#  language            :string(255)
+#  authoremail         :string(255)
+#  source_id           :integer
+#  quarantine          :boolean          default(FALSE)
+#  timestamp           :datetime
+#  contentsource       :string(255)
+#  import_record_id    :integer
+#  source_content_id   :string(255)
+#  parent_id           :integer
+#  content_category_id :integer
+#  category_reviewed   :boolean          default(FALSE)
+#  processed_content   :text
+#  has_event_calendar  :boolean          default(FALSE)
 #
 
 require 'fileutils'
@@ -86,9 +67,9 @@ class Content < ActiveRecord::Base
   belongs_to :content_category
 
   attr_accessible :title, :subtitle, :authors, :issue_id, :import_location_id, :copyright,
-                  :guid, :pubdate, :source_category, :topics, :summary, :url, :origin, :mimetype,
-                  :language, :page, :wordcount, :authoremail, :source_id, :file,
-                  :quarantine, :doctype, :timestamp, :contentsource, :source_content_id,
+                  :guid, :pubdate, :source_category, :topics, :summary, :url, 
+                  :language, :authoremail, :source_id, 
+                  :quarantine, :timestamp, :contentsource, :source_content_id,
                   :image_ids, :parent_id, :source_uri, :category,
                   :event_type, :start_date, :end_date, :cost, :recurrence, :host_organization,
                   :links, :featured, :content_category_id, :category_reviewed, :raw_content, :processed_content,
--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -2,54 +2,35 @@
 #
 # Table name: contents
 #
-#  id                   :integer          not null, primary key
-#  title                :string(255)
-#  subtitle             :string(255)
-#  authors              :string(255)
-#  raw_content          :text
-#  issue_id             :integer
-#  import_location_id   :integer
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
-#  copyright            :string(255)
-#  guid                 :string(255)
-#  pubdate              :datetime
-#  source_category      :string(255)
-#  topics               :string(255)
-#  summary              :text
-#  url                  :string(255)
-#  origin               :string(255)
-#  mimetype             :string(255)
-#  language             :string(255)
-#  page                 :string(255)
-#  wordcount            :string(255)
-#  authoremail          :string(255)
-#  source_id            :integer
-#  file                 :string(255)
-#  quarantine           :boolean          default(FALSE)
-#  doctype              :string(255)
-#  timestamp            :datetime
-#  contentsource        :string(255)
-#  import_record_id     :integer
-#  source_content_id    :string(255)
-#  parent_id            :integer
-#  event_type           :string(255)
-#  start_date           :datetime
-#  end_date             :datetime
-#  cost                 :string(255)
-#  recurrence           :string(255)
-#  links                :text
-#  host_organization    :string(255)
-#  business_location_id :integer
-#  featured             :boolean          default(FALSE)
-#  content_category_id  :integer
-#  category_reviewed    :boolean          default(FALSE)
-#  processed_content    :text
-#  event_title          :string(255)
-#  event_description    :text
-#  event_url            :string(255)
-#  sponsor_url          :string(255)
-#  has_event_calendar   :boolean          default(FALSE)
+#  id                  :integer          not null, primary key
+#  title               :string(255)
+#  subtitle            :string(255)
+#  authors             :string(255)
+#  raw_content         :text
+#  issue_id            :integer
+#  import_location_id  :integer
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  copyright           :string(255)
+#  guid                :string(255)
+#  pubdate             :datetime
+#  source_category     :string(255)
+#  topics              :string(255)
+#  summary             :text
+#  url                 :string(255)
+#  language            :string(255)
+#  authoremail         :string(255)
+#  source_id           :integer
+#  quarantine          :boolean          default(FALSE)
+#  timestamp           :datetime
+#  contentsource       :string(255)
+#  import_record_id    :integer
+#  source_content_id   :string(255)
+#  parent_id           :integer
+#  content_category_id :integer
+#  category_reviewed   :boolean          default(FALSE)
+#  processed_content   :text
+#  has_event_calendar  :boolean          default(FALSE)
 #
 
 require 'spec_helper'
