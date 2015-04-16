1c82ae2905104d5c956ffe31d1085705b5c08c07
John Stephens <john.stephens@i-intime.org>
2015-04-15 22:16:28 -0400

    admin-side support for moderation email

--- app/controllers/api/contents_controller.rb
+++ app/controllers/api/contents_controller.rb
@@ -223,6 +223,7 @@ class Api::ContentsController < Api::ApiController
     begin
     content = Content.find(params[:id])
     subject = 'dailyUV Flagged as ' + params[:classification] + ': ' +  content.title
+    params[:adminURL] = request.base_url
 
     ModerationMailer.send_moderation_flag(content, params, subject).deliver
 
