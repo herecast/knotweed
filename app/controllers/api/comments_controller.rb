d5c91ce96ca03858bf3349465912af9b62fb7999
Rich Cohen <rich.cohen@subtext.org>
2015-05-06 08:41:28 -0400

    Added loading images and updating contents_locations in comments controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -11,13 +11,26 @@ class Api::CommentsController < Api::ApiController
     cat_name = params[:content].delete :category
     cat = ContentCategory.find_or_create_by_name(cat_name) unless cat_name.nil?
 
+    # TODO: there has got to be a better way! but I tried simply piping the image straight through
+    # and allowing mass assignment to upload it as you would a normal form submission and no dice, so using
+    # JS's solution until we think of something better.
+    comment_image = nil
+    if params[:content][:image].present?
+      img = params[:content].delete :image
+      image_temp_file = Tempfile.new('tmpImage')
+      image_temp_file.puts img[:image_content]
+      file_to_upload = ActionDispatch::Http::UploadedFile.new(tempfile: image_temp_file,
+                                                              filename: img[:image_name], type: img[:image_type])
+      comment_image = Image.new image: file_to_upload
+    end
+
     # create content record to associate with the new comment
     content_record = {}
     content_record['title'] = params[:content].delete(:title)
     content_record['raw_content'] = params[:content].delete(:content)
     content_record['authors'] = params[:content].delete(:authors)
     content_record['authoremail'] = params[:content].delete(:authoremail)
-    # content_record['images'] = [market_post_image] if market_post_image.present?
+    content_record['images'] = [comment_image] if comment_image.present?
     content_record['content_category_id'] = cat.id
     content_record['publication_id'] = pub.id
     content_record['pubdate'] = content_record['timestamp'] = Time.zone.now
@@ -27,6 +40,12 @@ class Api::CommentsController < Api::ApiController
     @comment.content_attributes = content_record
 
     if @comment.save
+      # update content_locations
+      locations = params[:content].delete :locations
+      locations.each do |location_id|
+        ContentsLocationsHelper.do_insert_ids(@comment.content.id, location_id)
+      end
+
       # do reverse publishing if applicable
       if listservs.present?
         listservs.each do |d|
@@ -35,12 +54,13 @@ class Api::CommentsController < Api::ApiController
           PromotionListserv.create_from_content(@comment.content, list) if list.present? and list.active
         end
       end
+
       # regular publishing to DSP
       repo = Repository.find_by_dsp_endpoint(params[:repository])
       if repo.present? and @comment.publish(Content::DEFAULT_PUBLISH_METHOD, repo)
-        render text: "#{@comment.id}"
+        render text: "#{@comment.content.id}"
       else
-        render text: "Comment #{@comment.id} was created, but not published", status: 500
+        render text: "Comment #{@comment.content.id} was created, but not published", status: 500
       end
     else # if saving fails
       render text: "Comment could not be created", status: 500
