9553956c73f73c57bfdc3e9b3f05afff0477c8c8
Nick Gottlieb <ngottlieb@gmail.com>
2015-05-09 14:18:16 -0400

    added optional location_ids parameter to listservs index api call

--- app/controllers/api/listservs_controller.rb
+++ app/controllers/api/listservs_controller.rb
@@ -1,6 +1,12 @@
 class Api::ListservsController < Api::ApiController
   def index
-    @listservs = Listserv.all
+    if params[:location_ids].present?
+      location_ids = params[:location_ids].map{ |l| l.to_i }
+      @listservs = Listserv.joins('inner join listservs_locations')
+        .where('listservs_locations.location_id in (?)', location_ids)
+    else
+      @listservs = Listserv.all
+    end
     render json: @listservs
   end
 end
