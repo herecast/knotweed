0067fb402abbf075626f8dfb3cc5190376354fd3
jgstephe <johns@subtext.org>
2015-06-03 16:17:04 -0400

    enable sphinx indexing by setting skip_sphinx_rebuild to false

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -4,4 +4,4 @@ set :deploy_to, "/var/www/knotweed-admin-qa"
 server '97.107.134.192', roles: %w{web app db}, primary: true, user: 'deploy'
 
 set :restart_delayed_jobs, false
-set :skip_sphinx_rebuild, true
+set :skip_sphinx_rebuild, false 
