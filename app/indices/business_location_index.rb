301fd0c5d8a484f84a0d8200c02cad7f29cf8b28
Nick Gottlieb <ngottlieb@gmail.com>
2015-06-03 14:41:18 -0400

    change infix to prefix and sort search results by relevancy then name for locations

--- app/indices/business_location_index.rb
+++ app/indices/business_location_index.rb
@@ -1,8 +1,8 @@
 ThinkingSphinx::Index.define :business_location, :with => :active_record do
-  indexes :name
+  indexes :name, sortable: true
   indexes :city
   indexes :state
 
-  set_property :min_infix_len => 3
+  set_property :min_prefix_len => 3
   set_property :enable_star   => true
 end
