393df22869f6026c5cc53545b22aad15c3b96536
jgstephe <john.stephens@subtext.org>
2015-06-16 20:12:02 -0400

    Update promotion_banners_controller_spec.rb
    changed iterate to increment

--- spec/controllers/api/v2/promotion_banners_controller_spec.rb
+++ spec/controllers/api/v2/promotion_banners_controller_spec.rb
@@ -7,7 +7,7 @@ describe Api::V2::PromotionBannersController do
       @banner = FactoryGirl.create :promotion_banner
     end
 
-    it 'should iterate click_count' do
+    it 'should increment click_count' do
       count = @banner.click_count
       get :track_click, id: @banner.id
       @banner.reload.click_count.should eq(count+1)
