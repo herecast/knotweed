c1aca69d53ce1a7ae0d39310950c32e52ab726af
John Stephens <john.stephens@i-intime.org>
2015-08-06 22:45:54 -0400

    change handling of autocomplete query to pull at least 25 search results. Move the ones with the leftmost match to the top of the array, then slice it down to max_results.

--- app/controllers/api/v2/business_locations_controller.rb
+++ app/controllers/api/v2/business_locations_controller.rb
@@ -7,7 +7,12 @@ module Api
         if query.present?
           opts = {}
           opts = { select: '*, weight() as w', order: 'w DESC, name DESC' }
-          opts[:per_page] = params[:max_results] || 1000
+          if params[:autocomplete]
+            per_page = [params[:max_results], 25].max if params[:max_results].present?
+          else
+            per_page = params[:max_results]
+          end
+          opts[:per_page] = per_page || 1000
           opts[:star] = true
           @venues = BusinessLocation.search query, opts
         else
@@ -22,7 +27,15 @@ module Api
           # then sets cs to the most frequently occurring (city,state) pair
           cities = BusinessLocation.search(cs_query, cs_opts).map{|c| c.city.strip + ', ' + c.state.strip}
           cs = cities.group_by{|n| n }.values.max_by(&:size)
+
+          # reorganize the responses from the search so the locations that begin with the query
+          # string appear first, then follow with whatever is left and params[:max_results] results
           response_data = @venues.map{|v| "#{v.name} #{v.city}, #{v.state}".strip }
+          leftmost_matches = response_data.select{|v| v=~ /^#{query}/}.sort
+          response_data = leftmost_matches + (response_data - leftmost_matches)
+          response_data = response_data.slice(0..params[:max_results]-1) if params[:max_results].present?
+
+          # stick in the matched city at the top of the returned data
           response_data.prepend "#{cs.first}" if cs.present?
           render json: {
             locations: response_data
