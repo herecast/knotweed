1dd96be57a5e341c8ccb05018b03d545959bcb94
Wale Olaleye <subtext@railsfever.com>
2015-08-21 07:04:39 -0400

    set default values

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -1,5 +1,5 @@
 module AuthenticationHelpers
-  def api_authenticate(user,options={})
+  def api_authenticate(user,options={success:true})
     if options[:success]
       request.env['HTTP_AUTHORIZATION'] = \
         "Token token=#{user.authentication_token}, \
