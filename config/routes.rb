8cd422aabf97e5593852e1d74deff1e0bf47f661
Nick Gottlieb <ngottlieb@gmail.com>
2015-09-01 14:40:23 -0400

    add weather route for APIv3

--- config/routes.rb
+++ config/routes.rb
@@ -103,6 +103,7 @@ Knotweed::Application.routes.draw do
       resources 'talk', only: [:index, :show, :create, :update]
       resources 'market_posts', only: [:index, :show, :create, :update]
       get '/market_posts/:id/contact', to: 'market_posts#contact', as: :market_post_contact
+      get '/weather', to: 'users#weather', as: :weather
     end
 
     namespace :v2 do
