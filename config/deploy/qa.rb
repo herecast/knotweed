bd2362ba24ed2e794441c291422428e6fe8132ae
Nick Gottlieb <ngottlieb@gmail.com>
2015-09-03 09:52:54 -0600

    Update qa.rb
    reverted change to this file as it wasn't intended to be part of this commit

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -3,5 +3,5 @@ set :rails_env, :production
 set :deploy_to, "/var/www/knotweed-admin-qa"
 server '97.107.134.192', roles: %w{web app db}, primary: true, user: 'deploy'
 
-set :restart_delayed_jobs, true
+set :restart_delayed_jobs, false
 set :skip_sphinx_rebuild, true 
