2bfec02cfd730c374b639ed09d249a695bc0282a
Wale Olaleye <subtext@railsfever.com>
2015-09-08 08:13:02 -0400

    migrations and relationships for avatar added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150903170512) do
+ActiveRecord::Schema.define(:version => 20150908113810) do
 
   create_table "USGS_pop", :force => true do |t|
     t.integer "FEATURE_ID"
@@ -122,9 +122,9 @@ ActiveRecord::Schema.define(:version => 20150903170512) do
   add_index "category_tmp", ["content_id"], :name => "content_id"
 
   create_table "channel_map", :force => true do |t|
-    t.integer  "channel_id"
-    t.text     "category"
-    t.datetime "created_at", :null => false
+    t.integer   "channel_id"
+    t.text      "category"
+    t.timestamp "created_at", :null => false
   end
 
   add_index "channel_map", ["channel_id"], :name => "channel_id"
@@ -274,11 +274,11 @@ ActiveRecord::Schema.define(:version => 20150903170512) do
     t.integer  "channel_id"
     t.integer  "root_content_category_id"
     t.boolean  "delta",                    :default => true,  :null => false
-    t.integer  "created_by"
-    t.integer  "updated_by"
     t.integer  "view_count",               :default => 0
     t.integer  "comment_count",            :default => 0
     t.integer  "commenter_count",          :default => 0
+    t.integer  "created_by"
+    t.integer  "updated_by"
   end
 
   add_index "contents", ["authoremail"], :name => "index_contents_on_authoremail"
@@ -298,21 +298,6 @@ ActiveRecord::Schema.define(:version => 20150903170512) do
   add_index "contents", ["root_content_category_id"], :name => "index_contents_on_root_content_category_id"
   add_index "contents", ["source_category"], :name => "categories"
   add_index "contents", ["title"], :name => "title"
-
-  create_table "contents_NT", :force => true do |t|
-    t.string   "title"
-    t.string   "subtitle"
-    t.string   "authors"
-    t.string   "subject"
-    t.text     "content"
-    t.integer  "issue_id"
-    t.integer  "location_id"
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
-    t.boolean  "reviewed",       :default => false
-    t.integer  "lupdate_by"
-    t.integer  "publication_id"
-  end
 
   create_table "contents_events", :id => false, :force => true do |t|
     t.integer  "id",                                      :null => false
@@ -337,10 +322,6 @@ ActiveRecord::Schema.define(:version => 20150903170512) do
   add_index "contents_events", ["source_id"], :name => "source_id"
   add_index "contents_events", ["start_date"], :name => "index_contents_on_start_date"
   add_index "contents_events", ["title"], :name => "title"
-
-  create_table "contents_id", :force => true do |t|
-    t.string "category", :limit => 128
-  end
 
   create_table "contents_locations", :force => true do |t|
     t.integer  "content_id"
@@ -734,13 +715,6 @@ ActiveRecord::Schema.define(:version => 20150903170512) do
     t.datetime "updated_at",      :null => false
   end
 
-  create_table "promote_options", :force => true do |t|
-    t.string  "promo_type",            :limit => 128
-    t.string  "name",                  :limit => 128
-    t.string  "reverse_publish_email", :limit => 128
-    t.boolean "active",                               :default => true
-  end
-
   create_table "promotion_banners", :force => true do |t|
     t.string   "banner_image"
     t.string   "redirect_url"
@@ -929,6 +903,7 @@ ActiveRecord::Schema.define(:version => 20150903170512) do
     t.string   "test_group"
     t.boolean  "muted",                  :default => false
     t.string   "authentication_token"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
