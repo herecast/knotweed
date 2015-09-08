16c18687e13bcd7a526f7d04ad842d1793554dad
Nick Gottlieb <ngottlieb@gmail.com>
2015-09-08 12:10:06 -0400

    add specs for locations querying (APIv3) and removed zip code from the index

--- app/indices/location_index.rb
+++ app/indices/location_index.rb
@@ -1,7 +1,6 @@
 ThinkingSphinx::Index.define :location, with: :active_record do
   indexes :city
   indexes :state
-  indexes :zip
 
   set_property :min_prefix_len => 1
   set_property :enable_star => true
