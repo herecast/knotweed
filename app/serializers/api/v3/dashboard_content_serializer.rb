8b5aabe5fded8a1f24863ced99d394f00a728f90
Nick Gottlieb <ngottlieb@gmail.com>
2015-09-10 19:22:48 -0400

    added event_instance_id as id and event_id to dashboard serializer (if object is an event)

--- app/serializers/api/v3/dashboard_content_serializer.rb
+++ app/serializers/api/v3/dashboard_content_serializer.rb
@@ -3,7 +3,25 @@ module Api
     class DashboardContentSerializer < ActiveModel::Serializer
 
       attributes :id, :title, :parent_content_id, :content_type, :comment_count,
-        :view_count, :published_at
+        :view_count, :published_at, :event_id
+
+      def id
+        # if object is an event, Ember app needs an event instance ID
+        # as the ID. This will be revisited in the future, but for now, 
+        # is how Peter requested dashboard responses work.
+        if object.channel_type == 'Event'
+          object.channel.event_instances.first.id
+        else
+          object.id
+        end
+      end
+
+      # only set for event type objects
+      def event_id
+        if object.channel_type == 'Event'
+          object.channel_id
+        end
+      end
 
       def parent_content_id
         object.try(:parent).try(:id)
