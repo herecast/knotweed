28cacae4e86dfd78001a7069c6bc681e083282bd
John Stephens <john.stephens@i-intime.org>
2015-09-16 13:34:58 -0400

    changed send_moderation_flag to mirror send_moderation_flag_v2 and fixed specs to test.

--- spec/mailers/moderation_mailer_spec.rb
+++ spec/mailers/moderation_mailer_spec.rb
@@ -6,6 +6,7 @@ describe ModerationMailer do
       @content = FactoryGirl.create(:content)
       @content.authoremail = @content.authors + '@example.com'
       @content.save
+      @flagging_user = FactoryGirl.create(:user)
       @flagger_name = 'Joseph'
       @flagger_email = 'jos@example.com'
       @params = {:classification => 'Offensive', :flagger_name => @flagger_name, :flagger_email => @flagger_email }
@@ -14,13 +15,23 @@ describe ModerationMailer do
 
     it "should send an email" do
 
-      email = ModerationMailer.send_moderation_flag_v2(@content, @params, @subject).deliver
+      email = ModerationMailer.send_moderation_flag(@content, @params, @subject).deliver
       ModerationMailer.deliveries.present?.should== true
       email.body.include?(@flagger_name).should == true
       email.body.include?(@flagger_email).should == true
       email.body.include?(@content.authors).should == true
       email.body.include?(@content.authoremail).should == true
-      email.body.include?(@content.raw_content).should == true
+      email.to[0].should == ModerationMailer::MODERATION_EMAIL_RECIPIENT
+    end
+
+    it "should send a V2 email" do
+
+      email = ModerationMailer.send_moderation_flag_v2(@content, @params[:classification], @flagging_user).deliver
+      ModerationMailer.deliveries.present?.should== true
+      email.body.include?(@flagging_user.name).should == true
+      email.body.include?(@flagging_user.email).should == true
+      email.body.include?(@content.authors).should == true
+      email.body.include?(@content.authoremail).should == true
       email.to[0].should == ModerationMailer::MODERATION_EMAIL_RECIPIENT
     end
 
