efad1ab61a71dc01994af71509e09ee9ca976b21
Nick Gottlieb <ngottlieb@gmail.com>
2015-09-30 23:24:06 -0400

    adjust specs to test the specific email they actually reference

--- spec/mailers/reverse_publisher_spec.rb
+++ spec/mailers/reverse_publisher_spec.rb
@@ -11,28 +11,24 @@ describe ReversePublisher do
   describe 'after_create PromotionListserv object' do
     before do
       PromotionListserv.create_from_content(@content, @listserv)
+      ReversePublisher.deliveries.each do |eml|
+        # reverse publish email has this header set, confirmation email does not.
+        if eml['X-Original-Content-Id'].present?
+          @rp_email = eml
+        else
+          @conf_email = eml
+        end
+      end
     end
 
     it 'should send a confirmation email to the user' do
       ReversePublisher.deliveries.count.should == 2
-      # we don't know if conf is first or second so have to iterate through
-      should_pass = false
-      ReversePublisher.deliveries.each do |eml|
-        should_pass = eml.to.include? @content.authoremail
-        break if should_pass
-      end
-      should_pass.should == true
+      expect(@conf_email.to).to include(@content.authoremail)
     end
 
     it 'should send the reverse publish email to the listserv' do
       ReversePublisher.deliveries.count.should == 2
-      # we don't know if conf is first or second so have to iterate through
-      should_pass = false
-      ReversePublisher.deliveries.each do |eml|
-        should_pass = eml.to.include? @listserv.reverse_publish_email
-        break if should_pass
-      end
-      should_pass.should == true
+      expect(@rp_email.to).to include(@listserv.reverse_publish_email)
     end
   end
 
@@ -43,14 +39,12 @@ describe ReversePublisher do
       # stub out Thread.current for purposes of testing this
       allow(Thread).to receive(:current).and_return({consumer_app: FactoryGirl.create(:consumer_app)})
       @listserv.send_content_to_listserv(@content, Thread.current[:consumer_app])
+      # only the reverse publish email has this header, so use that to select it
+      @rp_email = ReversePublisher.deliveries.select{ |e| e['X-Original-Content-Id'].present? }.first
     end
+
     it 'should include the ux2 content path for @content' do
-      should_pass = false
-      ReversePublisher.deliveries.each do |eml|
-        should_pass = eml.body.include? "#{Thread.current[:consumer_app].uri}/news/#{@content.id}"
-        break if should_pass
-      end
-      expect(should_pass).to eq(true)
+      expect(@rp_email.body).to include("#{Thread.current[:consumer_app].uri}/news/#{@content.id}")
     end
   end
 
