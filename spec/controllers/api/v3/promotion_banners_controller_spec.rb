1fd173c0a530932a65c1b1c2fa68adb8079b0ba0
Wale Olaleye <subtext@railsfever.com>
2015-10-02 12:01:59 -0400

    change from get to post

--- spec/controllers/api/v3/promotion_banners_controller_spec.rb
+++ spec/controllers/api/v3/promotion_banners_controller_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe Api::V3::PromotionBannersController do
 
-  describe 'GET track_click' do
+  describe 'post track_click' do
     before do
       @banner = FactoryGirl.create :promotion_banner
       @content = FactoryGirl.create :content
       @content.promotion_banners = [@banner]
     end
 
-    subject! { get :track_click, promotion_banner_id: @banner.id,  content_id: @content.id, format: :json }
+    subject! { post :track_click, promotion_banner_id: @banner.id,  content_id: @content.id, format: :json }
 
     it 'should respond with 200' do
       response.status.should eq 200
@@ -24,14 +24,14 @@ describe Api::V3::PromotionBannersController do
     end
 
     context 'with invalid content id' do
-      subject! { get :track_click, promotion_banner_id: @banner.id, content_id: @content.id + 200, format: :json }
+      subject! { post :track_click, promotion_banner_id: @banner.id, content_id: @content.id + 200, format: :json }
       it 'should return 422' do
         response.status.should eq 422
       end
     end
 
     context 'with invalid promotion_banner_id' do
-      subject! { get :track_click, promotion_banner_id: @banner.id + 201, content_id: @content.id, format: :json }
+      subject! { post :track_click, promotion_banner_id: @banner.id + 201, content_id: @content.id, format: :json }
       it 'should return 422' do
         response.status.should eq 422
       end
