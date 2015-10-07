d865f105d125aabd019a7188c5540720d1126c0a
Wale Olaleye <subtext@railsfever.com>
2015-10-07 16:44:17 -0400

    added mixpanel tracking

--- app/controllers/api/v3/promotion_banners_controller.rb
+++ app/controllers/api/v3/promotion_banners_controller.rb
@@ -24,8 +24,23 @@ module Api
 
       def event_track
         props = {}
-        props['channelName'] = @content.channel_type
+        #nav props
+        props['channelName'] = @content.try(:channel_type)
+        props['url'] = url_for
 
+        #content props
+        props['contentId'] = @content.try(:id)
+        props['contentChannel'] = @content.try(:channel_type)
+        props['contentLocation'] = @content.try(:location)
+        props['contentPubdate'] = @content.try(:pubdate)
+        props['contentTitle'] = @content.try(:title)
+        props['contentPublicatio'] = @content.try(:publication).try(:name)
+        
+        #banner props
+        props['bannerAdId'] = @banner.try(:id)
+        props['bannerUrl'] = @banner.try(:redirect_url)
+        
+        @tracker.track(@current_api_user.try(:id), 'clickBannerAd', @current_api_user, props)
       end
 
     end
