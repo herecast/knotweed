03b2ee8e1386c4b3e63e5d7d221fc54674367e29
Wale Olaleye <subtext@railsfever.com>
2015-10-07 10:42:16 -0400

    enable code coverage collection if we are in CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
 require 'simplecov'
+
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
 SimpleCov.start 'rails' do
   add_group 'Serializers', 'app/serializers'
-end if ENV["COVERAGE"]
+end if ENV["COVERAGE"] || ENV["CI"]
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
