0c4d366bf6afb8bf72a6ea7dc5fa95e897b103ad
John O <johno@53tech.com>
2015-11-09 17:13:18 -0500

    Update qa.rb

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,7 +1,7 @@
 set :stage, :qa
 set :rails_env, :production
-set :deploy_to, "/var/www/knotweed-admin-qa"
-server '97.107.134.192', roles: %w{web app db}, primary: true, user: 'deploy'
+set :deploy_to, "/var/www/knotweed-admin"
+server '52.2.164.21', roles: %w{web app db}, primary: true, user: 'deploy'
 
 set :restart_delayed_jobs, false
 set :skip_sphinx_rebuild, true 
