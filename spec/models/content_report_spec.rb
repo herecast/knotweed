dfd0eaf25dcaac78f149853bd0481c7956c7e89b
Wale Olaleye <subtext@railsfever.com>
2015-11-23 17:28:44 -0500

    use build instead of create

--- spec/models/content_report_spec.rb
+++ spec/models/content_report_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe ContentReport do
-  before { @content_report = FactoryGirl.create :content_report }
+  before { @content_report = FactoryGirl.build :content_report }
   subject { @content_report }
   it { should be_valid }
 end
