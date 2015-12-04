642f337e7745e584abecdf9c1d2c244fde99846f
Nick Gottlieb <ngottlieb@gmail.com>
2015-12-04 13:58:36 -0500

    update cap staging stage to point to elastic ip instead of stage-web

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :stage, :staging
 set :rails_env, 'production'
 set :deploy_to, "/var/www/knotweed-admin"
-server 'stage-web.subtext.org', roles: %w{web app db}, primary: true, user: 'deploy'
+server '52.70.81.201', roles: %w{web app db}, primary: true, user: 'deploy'
 
 set :restart_delayed_jobs, true
