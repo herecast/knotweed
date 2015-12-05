9991889de23c56f6991553fce852e559be6f048e
Nick Gottlieb <ngottlieb@gmail.com>
2015-12-05 14:54:54 -0500

    added edit capacity for schedules

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,7 +3,7 @@ class Schedule < ActiveRecord::Base
   belongs_to :event
   attr_accessible :recurrence, :description_override, :subtitle_override, :presenter_name
 
-  after_create :create_event_instances
+  after_save :update_event_instances
 
   def schedule
     if recurrence.present?
@@ -17,18 +17,42 @@ class Schedule < ActiveRecord::Base
     update_attribute :recurrence, sched.to_yaml
   end
 
-  # create event_instances for schedule
-  def create_event_instances
+  # returns hash keyed by date objects for each event instance
+  def event_instances_by_date
+    hash = {}
+    event_instances.each do |ei| 
+      hash[ei.start_date] = ei
+    end
+    hash
+  end
+
+  # create or update event_instances for schedule
+  def update_event_instances
     if schedule.present?
+      # remove no longer existent occurrences
+      event_instances.each do |ei|
+        ei.destroy unless schedule.all_occurrences.include? ei.start_date
+      end
+      eis_by_date = event_instances_by_date
+      # add new occurrences
       schedule.each_occurrence do |date|
-        EventInstance.create(
-          schedule_id: self.id, 
-          event_id: event.id,
-          start_date: date,
-          description_override: description_override,
+        unless eis_by_date.has_key? date
+          EventInstance.create(
+            schedule_id: self.id, 
+            event_id: event.id,
+            start_date: date,
+            description_override: description_override,
+            subtitle_override: subtitle_override,
+            presenter_name: presenter_name
+          )
+        end
+      end
+      if description_override_changed? or subtitle_override_changed? or presenter_name_changed?
+        event_instances.update_all({
+          presenter_name: presenter_name,
           subtitle_override: subtitle_override,
-          presenter_name: presenter_name
-        )
+          description_override: description_override
+        })
       end
     end
     event_instances
--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,29 +1,90 @@
 require 'spec_helper'
 
 describe Schedule do
-  describe 'after_create :create_event_instances' do
-    before do
-      @recurrence = IceCube::Schedule.new(Time.now) do |s|
-        s.add_recurrence_rule IceCube::Rule.daily.until 1.week.from_now
+  describe 'after_save :update_event_instances' do
+
+    describe 'on creation' do
+      before do
+        @recurrence = IceCube::Schedule.new(Time.now) do |s|
+          s.add_recurrence_rule IceCube::Rule.daily.until 1.week.from_now
+        end
+        @event = FactoryGirl.create :event # note, creates an event instance
       end
-      @event = FactoryGirl.create :event # note, creates an event instance
+
+      subject { FactoryGirl.create :schedule, recurrence: @recurrence.to_yaml, event: @event }
+
+      it 'should create the right number of event instances' do
+        expect{subject}.to change{EventInstance.count}.by(@recurrence.all_occurrences.count)
+      end
+
+      it 'should associate the correct number of new event_instances with the event' do
+        expect{subject}.to change{@event.event_instances.count}.by @recurrence.all_occurrences.count
+      end
+
+      it 'should associate the new event_instances with the schedule and the event' do
+        subject
+        sched = Schedule.last
+        sched.event_instances.count.should eq(@recurrence.all_occurrences.count)
+      end
     end
 
-    subject { FactoryGirl.create :schedule, recurrence: @recurrence.to_yaml, event: @event }
+    describe 'on update' do
+      before do
+        @recurrence = IceCube::Schedule.new(Time.now) do |s|
+          s.add_recurrence_rule IceCube::Rule.daily.until 2.weeks.from_now
+        end
+        @event = FactoryGirl.create :event
+        @schedule = FactoryGirl.create :schedule, recurrence: @recurrence.to_yaml, event: @event
+        @schedule.reload
+      end
 
-    it 'should create the right number of event instances' do
-      expect{subject}.to change{EventInstance.count}.by(@recurrence.all_occurrences.count)
+      subject do 
+        @schedule.recurrence = IceCube::Schedule.new(Time.now) do |s|
+          s.add_recurrence_rule IceCube::Rule.weekly.until 2.weeks.from_now
+        end.to_yaml
+        @schedule.save
+      end
+
+      it 'should change the number of event instances associated with the schedule' do
+        expect{subject}.to change{@schedule.event_instances.count}.by -12
+      end
+
+      it 'should change the number of event instances associated with the event' do
+        expect{subject}.to change{@event.event_instances.count}.by -12
+      end
+
+      it 'should change the number of event instances' do
+        expect{subject}.to change{EventInstance.count}.by -12
+      end
     end
 
-    it 'should associate the correct number of new event_instances with the event' do
-      expect{subject}.to change{@event.event_instances.count}.by @recurrence.all_occurrences.count
+    describe 'updating event instance attributes' do
+      before do
+        @recurrence = IceCube::Schedule.new(Time.now) do |s|
+          s.add_recurrence_rule IceCube::Rule.daily.until 2.weeks.from_now
+        end
+        @event = FactoryGirl.create :event
+        @schedule = FactoryGirl.create :schedule, recurrence: @recurrence.to_yaml, event: @event,
+          description_override: 'zilch', presenter_name: 'blargh', subtitle_override: 'nom'
+      end
+
+      subject do
+        @schedule.description_override = 'New override'
+        @schedule.presenter_name = 'Test McTester'
+        @schedule.subtitle_override = 'new subtitle'
+        @schedule.save
+      end
+
+      it 'should update the associated event instance attributes' do
+        subject
+        @schedule.reload
+        example_instance = @schedule.event_instances.first
+        example_instance.presenter_name.should eq @schedule.presenter_name
+        example_instance.subtitle_override.should eq @schedule.subtitle_override
+        example_instance.description_override.should eq @schedule.description_override
+      end
     end
 
-    it 'should associate the new event_instances with the schedule and the event' do
-      subject
-      sched = Schedule.last
-      sched.event_instances.count.should eq(@recurrence.all_occurrences.count)
-    end
   end
 
 end
