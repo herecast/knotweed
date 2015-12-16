1a6f1a88cd349e56d759411be68bedf2d9823464
Nick Gottlieb <ngottlieb@gmail.com>
2015-12-16 14:17:18 -0500

    monkey patch ice cube to remove dst_adjustment of occurrence times

--- config/initializers/ice_cube.rb
+++ config/initializers/ice_cube.rb
@@ -1,2 +1,10 @@
 # avoid deprecation warnings about syntax we're not using
 IceCube.compatibility = 12
+
+module IceCube
+  class ValidatedRule
+    def dst_adjust?
+      false
+    end
+  end
+end
