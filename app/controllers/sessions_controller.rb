c00b1c62f271cc4ba3845d4456931c6a33ea23cd
Nik Petersen <demersus@gmail.com>
2016-01-28 16:27:51 -0500

    Removed Mixpanel tracking from signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < Devise::SessionsController
   respond_to :html, :json
-  after_filter :track, only: :create
 
   def create
     super do |user|
@@ -13,13 +12,4 @@ class SessionsController < Devise::SessionsController
       end
     end
   end
-
-  private
-
-  def track
-    @tracker ||= SubtextTracker.new(Figaro.env.mixpanel_api_token)
-    @mixpanel_distinct_id = current_user.try(:id) || request.headers['Mixpanel-Distinct-Id']
-    @tracker.track(@mixpanel_distinct_id, 'signIn', current_user, Hash.new)
-  end
-
 end
