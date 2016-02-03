88cf12a0c3a55fadd848aff04cf4073482009c36
Nick Gottlieb <ngottlieb@gmail.com>
2016-02-03 15:41:52 -0500

    limit serializer _reports responses to most recent 30

--- app/serializers/api/v3/promotion_banner_metrics_serializer.rb
+++ app/serializers/api/v3/promotion_banner_metrics_serializer.rb
@@ -13,7 +13,9 @@ module Api
       def image_url; object.banner_image.url; end
 
       def daily_impression_counts
-        object.promotion_banner_reports.map do |report|
+        # NOTE, I don't love performing the `limit` here but it's just temporary.
+        # We want to implement sorting, paging through the API down the road.
+        object.promotion_banner_reports.limit(30).map do |report|
           {
             report_date: report.report_date,
             impression_count: report.impression_count
@@ -22,7 +24,7 @@ module Api
       end
 
       def daily_click_counts
-        object.promotion_banner_reports.map do |report|
+        object.promotion_banner_reports.limit(30).map do |report|
           {
             report_date: report.report_date,
             click_count: report.click_count
