0fa7ee707c4cd2e554d87b7db375d6b9d6e706ba
Nik Petersen <demersus@gmail.com>
2016-03-31 12:15:37 -0400

    Increase test coverage of contents helper

--- spec/helpers/contents_helper_spec.rb
+++ spec/helpers/contents_helper_spec.rb
@@ -21,5 +21,118 @@ describe ContentsHelper, type: :helper do
     end
 
   end
+
+  describe '#search_field_value' do
+    let(:key) { :a_key }
+    subject { helper.search_field_value(key) }
+
+    context 'params[:reset]' do
+      it { should be nil }
+    end
+
+    context 'session[:contents_search]' do
+      before do
+        session[:contents_search] = {key => 'a_value'}
+      end
+
+      it 'returns session[:contents_search][key]' do
+        expect(subject).to eql 'a_value'
+      end
+    end
+
+    context 'params[:q]' do
+      before do
+        params[:q] = { key => 'a_value1' }
+      end
+
+      it 'returns params[:q][key]' do
+        expect(subject).to eql 'a_value1'
+      end
+    end
+  end
+
+  describe '#remove_list_from_title' do
+    context 'When title has [bracket content]' do
+      subject{ helper.remove_list_from_title("My Title [in brackets]") }
+
+      it 'removes [bracket content]' do
+        expect(subject).to_not include('[in brackets]')
+      end
+    end
+  end
+
+  describe '#can_be_listserv_promoted' do
+    let(:content) { Content.new }
+    before do
+      content.authors = [1]
+      content.authoremail = 'test@maildrop.cc'
+      content.title = 'the title'
+    end
+    subject { helper.can_be_listserv_promoted(content) }
+
+    context 'When authors, authoremail, and title' do
+      it { should be true }
+    end
+
+    context 'When authors empty' do
+      before do
+        content.authors = []
+      end
+
+      it { should be false }
+    end
+
+    context 'When authoremail blank' do
+      before do
+        content.authoremail = ""
+      end
+
+      it { should be false }
+    end
+
+    context 'When title blank' do
+      before do
+        content.title = ""
+      end
+
+      it { should be false }
+    end
+  end
+
+  describe '#content_url_for_email' do
+    let(:content) { FactoryGirl.create :content }
+    subject { helper.content_url_for_email(content) }
+    let(:content_path) { ux2_content_path(content) }
+    let(:utm_string) { "?utm_medium=email&utm_source=rev-pub&utm_campaign=20151201&utm_content=#{content_path}" }
+
+    context 'consumer_app set from request' do
+      let(:consumer_app) { double('consumer_app', uri: 'http://my-uri.example') }
+      before do
+        Thread.current[:consumer_app] = consumer_app
+      end
+
+      it { should eql "#{consumer_app.uri}#{content_path}#{utm_string}" }
+    end
+
+    context 'consumer_app not set; @base_uri set from controller' do
+      before do
+        @base_uri = 'http://event.foo'
+        Thread.current[:consumer_app] = nil
+      end
+
+      it 'uses @base_uri' do
+        expect(subject).to eql "#{@base_uri}/contents/#{content.id}#{utm_string}"
+      end
+    end
+
+    context 'if not consumer_app, or @base_uri;' do
+      before do 
+        @base_uri = nil
+        Thread.current[:consumer_app] = nil
+      end
+
+      it { should eql "http://www.dailyuv.com/contents/#{content.id}" }
+    end
+  end
 end
 
