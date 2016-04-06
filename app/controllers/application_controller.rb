de84312d15d2cf1359ce5eec9fa5e2297c1b6d2a
Nick Gottlieb <ngottlieb@gmail.com>
2016-04-06 16:23:11 -0400

    monkeypatch workaround for rspec rails 3.2.22 issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,8 @@ class ApplicationController < ActionController::Base
       logger.info "Rendering 404: #{exception.message}"
     end
 
-    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+    render :file => "#{Rails.root}/public/404.html",
+      :status => 404, :layout => false
   end
 
   def set_current_thread_user
