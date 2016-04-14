8bd3290ed43ca1779a9f54c9a0b3aa33fdfc5781
Jordan Metzmeier <titan8990@gmail.com>
2016-04-14 15:14:40 -0500

    add rvm_roles for staging and dev

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,5 +1,6 @@
 set :rails_env, 'production'
 set :deploy_to, "/var/www/knotweed-admin"
+set :rvm_roles, [:web, :app]
 server 'dev.subtext.org', roles: %w{web app db}, primary: true, user: 'deploy'
 server 'test-dsp.subtext.org', roles: %w{source_dsp}, primary: false, user: 'dsp'
 server 'dev-dsp.subtext.org', roles: %w{dest_dsp}, primary: false, user: 'dsp'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,6 @@
 set :stage, :staging
 set :rails_env, 'production'
+set :rvm_roles, [:web, :app]
 set :deploy_to, "/var/www/knotweed-admin"
 server '52.70.81.201', roles: %w{web app db}, primary: true, user: 'deploy'
 server 'test-dsp.subtext.org', roles: %w{source_dsp}, primary: false, user: 'dsp'
