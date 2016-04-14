71d4031a748d87d24ed923e9d844e7e17067c10f
Jordan Metzmeier <titan8990@gmail.com>
2016-04-14 15:13:33 -0500

    add refresh dsp server configs for dev and staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,5 +2,7 @@ set :stage, :staging
 set :rails_env, 'production'
 set :deploy_to, "/var/www/knotweed-admin"
 server '52.70.81.201', roles: %w{web app db}, primary: true, user: 'deploy'
+server 'test-dsp.subtext.org', roles: %w{source_dsp}, primary: false, user: 'dsp'
+server 'stage-dsp.subtext.org', roles: %w{dest_dsp}, primary: false, user: 'dsp'
 
 set :restart_delayed_jobs, true
