42b78dd507d4ea920a4d089b3c3687fb21658309
Benjamin Sheehan <ben.sheehan@subtext.org>
2016-04-25 12:24:18 -0400

    Functional in case file is missing

--- app/validators/image_minimum_size_validator.rb
+++ app/validators/image_minimum_size_validator.rb
@@ -1,6 +1,7 @@
 class ImageMinimumSizeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless record.send(attribute).current_path.nil?
+    file = record.send(attribute).current_path
+    unless file.nil? or !File.exist?(file)
       unless Dimensions.width(record.send(attribute).current_path) >= 200 and Dimensions.height(record.send(attribute).current_path) >= 200
         record.errors[attribute] << "The image is not large enough"
       end
