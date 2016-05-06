e38397575bc8901ab97bb74f9ed12a52957b6cfd
Nik Petersen <demersus@gmail.com>
2016-05-05 22:55:32 -0700

    Revert "Revert "add my_town_only to market post serializer""
    This reverts commit feb04306ffbf13cbb3360f6570420ff4825e551d.

--- app/serializers/api/v3/market_post_serializer.rb
+++ app/serializers/api/v3/market_post_serializer.rb
@@ -3,7 +3,7 @@ module Api
     # note, this serializer actually takes content objects, not market post objects
     class MarketPostSerializer < ActiveModel::Serializer
 
-      attributes :id, :title, :published_at, :image_url, :content_id
+      attributes :id, :title, :published_at, :image_url, :content_id, :my_town_only
 
       def content_id
         object.id
