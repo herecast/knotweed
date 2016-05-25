5933b56e6474d239d63d142cae4e9a9998a9588a
Jordan Metzmeier <titan8990@gmail.com>
2016-05-25 08:54:29 -0500

    update consumer_apps table on database refresh

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,6 +7,7 @@ set :dest_mysql_host, 'stage-004.cogj3v9uqpkv.us-east-1.rds.amazonaws.com'
 set :source_mysql_database, 'admin_prod'
 set :dest_mysql_database, 'admin_prod'
 set :dsp_endpoint_hostname, 'stage-dsp.subtext.org'
+set :site_endpoint, 'http://stage-consumer.subtext.org'
 server '52.70.81.201', roles: %w{web app db}, primary: true, user: 'deploy'
 server 'test-dsp.subtext.org', roles: %w{source_dsp}, primary: false, user: 'dsp', no_release: true
 server 'stage-dsp.subtext.org', roles: %w{dest_dsp}, primary: false, user: 'dsp', no_release: true
