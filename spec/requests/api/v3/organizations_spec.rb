81103cc51bcf8e71ebd8b3be5a6199bf23f7d1d9
Nik Petersen <demersus@gmail.com>
2016-06-10 09:59:20 -0700

    updated organization endpoint to return correct id for business profile (#719)

--- spec/requests/api/v3/organizations_spec.rb
+++ spec/requests/api/v3/organizations_spec.rb
@@ -1,8 +1,42 @@
 require 'spec_helper'
 
-RSpec.describe 'Organizations Endpoints' do
+RSpec.describe 'Organizations Endpoints', type: :request do
   describe 'GET /api/v3/organizations/:id' do
     let!(:organization) { FactoryGirl.create :organization }
+
+    it 'responds with organization json' do
+      get "/api/v3/organizations/#{organization.id}"
+      expect(response_json).to match(
+        organization: {
+          id: organization.id,
+          name: organization.name,
+          can_publish_news: organization.can_publish_news,
+          subscribe_url: organization.subscribe_url,
+          logo_url: an_instance_of(String).or(be_nil),
+          business_profile_id: a_kind_of(Fixnum).or(be_nil),
+          description: organization.description,
+          org_type: organization.org_type,
+          can_edit: boolean
+        }
+      )
+    end
+
+    context 'with claimed business_profile' do
+      let!(:business_profile) { FactoryGirl.create :business_profile, :claimed, id: 100 }
+      before do
+        business_profile.content.organization= organization
+        business_profile.content.save!
+      end
+
+      describe 'business_profile_id' do
+        subject { response_json[:organization][:business_profile_id] }
+
+        it 'is business_profile.id' do
+          get "/api/v3/organizations/#{organization.id}"
+          expect(subject).to eql business_profile.id
+        end
+      end
+    end
 
     describe 'can_edit' do
       subject { response_json[:organization][:can_edit] }
