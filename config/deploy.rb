292a89bcbed8b8dc0c75f6fe0e938290db031cfe
Jordan Metzmeier <titan8990@gmail.com>
2016-07-07 14:49:48 -0400

    add newrelic.yml to linked_file (#802)

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :git_strategy, SubmoduleStrategy
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/application.yml config/production.sphinx.conf config/thinking_sphinx.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/production.sphinx.conf config/thinking_sphinx.yml config/newrelic.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets public/exports binlog db/sphinx}
