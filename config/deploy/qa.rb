4346ac472af053e182cc4db73b1b1045ec915f42
Jordan Metzmeier <titan8990@gmail.com>
2016-07-28 12:14:19 -0400

    Postgres refresh (#848)
    * changes needed for postgres refreshes
    
    * drop -c for pg_dump and add -x
    
    * update postgres database URLs
    
    * use -c --if-exists for pg_dump
    
    * revert changes to Gemfile.lock
    
    * rename variables for QA refresh

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -2,10 +2,10 @@ set :stage, :qa
 set :rails_env, :production
 set :rvm_roles, [:web, :app]
 set :deploy_to, "/var/www/knotweed-admin"
-set :source_mysql_host, 'reports.subtext.org'
-set :dest_mysql_host, 'qa-004.cogj3v9uqpkv.us-east-1.rds.amazonaws.com'
-set :source_mysql_database, 'admin_prod'
-set :dest_mysql_database, 'admin_prod'
+set :source_database_host, 'reports.subtext.org'
+set :dest_database_host, 'qa-005.cogj3v9uqpkv.us-east-1.rds.amazonaws.com'
+set :source_database, 'knotweed'
+set :dest_database, 'knotweed'
 set :dsp_endpoint_hostname, 'dev-dsp.subtext.org'
 set :site_endpoint, 'http://qa-consumer.subtext.org'
 server 'qa-web.subtext.org', roles: %w{web app db}, primary: true, user: 'deploy'
