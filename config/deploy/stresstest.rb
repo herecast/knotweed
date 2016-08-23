dab62ea42bfda3b3eb9d0d30bf94afcf11c87f17
Jordan Metzmeier <titan8990@gmail.com>
2016-08-23 09:18:00 -0400

    remove unused deploy configs (#899)

--- config/deploy/stresstest.rb
+++
@@ -1,7 +0,0 @@
-set :stage, :stresstest
-set :rails_env, :production
-server '97.107.134.192', roles: %w{web app db}, primary: true, user: 'deploy'
-set :deploy_to, "/var/www/knotweed-admin-stresstest"
-
-set :restart_delayed_jobs, false
-set :restart_sidekiq, false
