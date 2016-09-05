5712fe0108785c2600604b460ef2a73629ead750
sheehan1102 <ben.sheehan@subtext.org>
2016-09-05 14:16:38 -0400

    Adjusting Elasticsearch signing conditional (#931)
    * Adjusting Elasticsearch signing conditional
    
    * Correcting conditional
    
    * Add argument to present?

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -7,11 +7,13 @@ Searchkick.client = Elasticsearch::Client.new(
   retry_on_failure: true # https://github.com/ankane/searchkick/issues/351
 ) do |f|
   # we don't want to attempt to sign requests to our local elasticsearch instance on dev
-  unless ENV["ELASTICSEARCH_URL"].present? and ENV["ELASTICSEARCH_URL"].include? 'localhost'
-    f.request :aws_signers_v4, {
-      credentials: Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"]),
-      service_name: "es",
-      region: "us-east-1"
-    }
+  unless ENV["ELASTICSEARCH_URL"].present? && ENV["ELASTICSEARCH_URL"].include? 'localhost'
+    if ENV["ELASTICSEARCH_URL"].include? 'es.amazonaws.com'
+      f.request :aws_signers_v4, {
+        credentials: Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"]),
+        service_name: "es",
+        region: "us-east-1"
+      }
+    end
   end
 end
