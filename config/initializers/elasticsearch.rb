c2fd231139c7e326de9cfbab7057d0117333e00c
sheehan1102 <ben.sheehan@subtext.org>
2016-09-07 09:44:49 -0400

    Allow for empty ELASTICSEARCH_URL in initializer (#933)

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -8,7 +8,7 @@ Searchkick.client = Elasticsearch::Client.new(
 ) do |f|
   # we don't want to attempt to sign requests to our local elasticsearch instance on dev
   unless ENV["ELASTICSEARCH_URL"].present? and ENV["ELASTICSEARCH_URL"].include? 'localhost'
-    if ENV["ELASTICSEARCH_URL"].include? 'es.amazonaws.com'
+    if ENV["ELASTICSEARCH_URL"].present? and ENV["ELASTICSEARCH_URL"].include? 'es.amazonaws.com'
       f.request :aws_signers_v4, {
         credentials: Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"]),
         service_name: "es",
