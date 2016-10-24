eeb719a2dc0e15050e314e5ee24dd2cd456f93d7
Nick Gottlieb <ngottlieb@gmail.com>
2016-10-24 13:35:29 -0400

    remove all dev ops / deploy references to delayed job

--- config/initializers/delayed_job.rb
+++
@@ -1,5 +0,0 @@
-Delayed::Worker.backend = :active_record
-Delayed::Worker.max_attempts = 5
-Delayed::Worker.max_run_time = 1.month
-Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'), 10, 100*1024*1024)
-# Delayed::Worker.delay_jobs = !Rails.env.test?
