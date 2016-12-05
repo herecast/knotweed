67cd3bc557960743f08f1545ef17ea2e1f53adcb
Nik Petersen <demersus@gmail.com>
2016-12-05 13:36:40 -0800

    remove preview from non existent email

--- spec/mailers/previews/listserv_preview.rb
+++ spec/mailers/previews/listserv_preview.rb
@@ -4,10 +4,6 @@ class ListservPreview < ActionMailer::Preview
     ListservMailer.subscription_verification(Subscription.last)
   end
 
-  def subscription_confirmation
-    ListservMailer.subscription_confirmation(Subscription.last)
-  end
-
   def existing_subscription
     ListservMailer.existing_subscription(Subscription.last)
   end
