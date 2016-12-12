ab43128c941883ec1b161fb8b0a9545993f99f0d
sheehan1102 <ben.sheehan@subtext.org>
2016-12-12 11:18:24 -0500

    Ad report needs historical totals, not current totals (#1126)

--- app/controllers/api/v3/promotion_banner_reports_controller.rb
+++ app/controllers/api/v3/promotion_banner_reports_controller.rb
@@ -6,10 +6,10 @@ module Api
         query = "SELECT p.id as \"promotion_id\", c.title, p.paid, date_trunc('day', pbr.report_date) AS \"report_date\",
           pbr.impression_count as \"daily_impression_count\", pbr.click_count as \"daily_click_count\",
           (
-            SELECT SUM(impression_count) FROM promotion_banner_reports WHERE promotion_banner_id = pb.id
+            SELECT SUM(impression_count) FROM promotion_banner_reports WHERE promotion_banner_id = pb.id AND date_trunc('day', report_date + INTERVAL '5 hours') < CURRENT_DATE
           ) as total_impression_count,
           (
-            SELECT SUM(click_count) FROM promotion_banner_reports WHERE promotion_banner_id = pb.id
+            SELECT SUM(click_count) FROM promotion_banner_reports WHERE promotion_banner_id = pb.id AND date_trunc('day', report_date + INTERVAL '5 hours') < CURRENT_DATE
           ) as total_click_count,
           pb.campaign_start, pb.campaign_end,
           pb.max_impressions, p.content_id, pbr.promotion_banner_id
@@ -17,7 +17,7 @@ module Api
           INNER JOIN promotion_banners pb ON pbr.promotion_banner_id = pb.id
           INNER JOIN promotions p ON p.promotable_type = 'PromotionBanner' AND p.promotable_id = pb.id
           INNER JOIN contents c ON p.content_id = c.id
-          WHERE date_trunc('day', pbr.report_date) < CURRENT_DATE
+          WHERE date_trunc('day', pbr.report_date + INTERVAL '5 hours') < CURRENT_DATE
           ORDER BY DATE(report_date) DESC, campaign_start DESC, p.id DESC;"
         @promotion_banner_reports = ActiveRecord::Base.connection.execute(query)
 
