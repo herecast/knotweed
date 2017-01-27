cc63552476ef5695a192854d9e063d8996a75521
Nick Gottlieb <ngottlieb@gmail.com>
2017-01-27 11:35:11 -0700

    Feature/remove capistrano (#1184)
    * remove capistrano
    
    * remove mysql2

--- config/deploy/production.rb
+++
@@ -1,6 +0,0 @@
-set :rails_env, 'production'
-set :deploy_to, "/var/www/knotweed-admin"
-server 'admin.subtext.org', roles: %w{web app db}, primary: true, user: 'deploy'
-
-set :restart_sidekiq, true
-set :robots_file, 'config/public_robots.txt'
