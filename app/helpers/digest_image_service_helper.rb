3b9aa17bc3866ae863bfa8b9f7ade110971c900c
Cody Norman <wcodynorman@gmail.com>
2017-05-19 08:03:12 -0600

    Feature/update news template thumbnails (#1280)
    * added height and width to img styles
    
    * updated digest image helpers to accept width and height
    
    * add keyword splat to digest_banner_ad_image
    
    * adjusted image width and height to try to address scaling problems in some email clients
    
    * add back removed doctype tag

--- app/helpers/digest_image_service_helper.rb
+++ app/helpers/digest_image_service_helper.rb
@@ -1,22 +1,22 @@
 module DigestImageServiceHelper
-  def digest_primary_image(image_url)
+  def digest_primary_image(image_url, **image_attrs)
     ImageUrlService.optimize_image_url(url: image_url,
-                                       width: nil,
-                                       height: nil,
+                                       width: image_attrs[:width],
+                                       height: image_attrs[:height],
                                        do_crop: false)
   end
 
-  def digest_profile_image(image_url)
+  def digest_profile_image(image_url, **image_attrs)
     ImageUrlService.optimize_image_url(url: image_url,
-                                       width: nil,
-                                       height: nil,
+                                       width: image_attrs[:width],
+                                       height: image_attrs[:height],
                                        do_crop: false)
   end
 
-  def digest_banner_ad_image(image_url)
+  def digest_banner_ad_image(image_url, **image_attrs)
     ImageUrlService.optimize_image_url(url: image_url,
-                                       width: nil,
-                                       height: nil,
+                                       width: image_attrs[:width],
+                                       height: image_attrs[:height],
                                        do_crop: false)
   end
 end
