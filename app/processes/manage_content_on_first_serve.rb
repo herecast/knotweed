1112a67541e2a68f290ce6c28f5d9f94538a2213
sheehan1102 <ben.sheehan@subtext.org>
2018-08-29 11:50:11 -0400

    Prevent double send of subscriber emails (#1588)

--- app/processes/manage_content_on_first_serve.rb
+++ app/processes/manage_content_on_first_serve.rb
@@ -15,14 +15,17 @@ class ManageContentOnFirstServe
                          .where('pubdate < ?', Time.current)
 
     @contents.each do |content|
-      content.update_attribute(:first_served_at, @current_time)
-      conditionally_schedule_outreach(content)
-      conditionally_schedule_subscriber_notification(content)
-      if Figaro.env.production_messaging_enabled == "true"
-        FacebookService.rescrape_url(content)
-        if content.content_type == :news
-          IntercomService.send_published_content_event(content)
-          SlackService.send_published_content_notification(content)
+      # extra check to avoid race condition
+      if content.first_served_at.nil?
+        content.update_attribute(:first_served_at, @current_time)
+        conditionally_schedule_outreach(content)
+        conditionally_schedule_subscriber_notification(content)
+        if Figaro.env.production_messaging_enabled == "true"
+          FacebookService.rescrape_url(content)
+          if content.content_type == :news
+            IntercomService.send_published_content_event(content)
+            SlackService.send_published_content_notification(content)
+          end
         end
       end
     end
