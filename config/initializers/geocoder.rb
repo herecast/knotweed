b044bb9cc0345313a4881445c193809606910168
sheehan1102 <ben.sheehan@subtext.org>
2018-11-07 09:22:55 -0500

    [ch15566] update geocoding service (#1634)
    * [ch15566] update geocoding service
    
    * [ch15566] try different service
    
    * [ch15566] return to google geocoding w/ api key

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -10,7 +10,7 @@ end
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
-  lookup: :google,            # name of geocoding service (symbol)
+  lookup: :google,              # name of geocoding service (symbol)
   ip_lookup: :freegeoip,
   freegeoip: {
     host: "freegeoip.io",
@@ -20,7 +20,7 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: nil,               # API key for geocoding service
+  api_key: Figaro.env.gmaps_api_key,               # API key for geocoding service
   cache: Redis.new                 # cache object (must respond to #[], #[]=, and #keys)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
