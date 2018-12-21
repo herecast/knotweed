05a12d59b5e4cf4736b48066b00dc7df41f0c4e5
Nick Gottlieb <ngottlieb@gmail.com>
2018-12-21 08:13:41 -0800

    [ch16154] Feature/ch16154/improve service spec coverage (#1673)
    * [ch16154] add intercom service spec
    
    * [ch16154] add some very basic bill_dot_com_service testing

--- spec/services/bill_dot_com_service_spec.rb
+++ spec/services/bill_dot_com_service_spec.rb
@@ -3,4 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe BillDotComService do
+
+  subject { BillDotComService }
+
+  it { is_expected.to respond_to(:authenticate) }
+  it { is_expected.to respond_to(:send_payment) }
+  it { is_expected.to respond_to(:find_vendor) }
+  it { is_expected.to respond_to(:create_bill) }
 end
---
+++ spec/services/intercom_service_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe IntercomService do
+
+  subject { IntercomService }
+
+  it { is_expected.to respond_to(:send_published_content_event) }
+
+  describe '#send_published_content_event', freeze_time: true do
+    let(:content) { FactoryGirl.create :content }
+    let(:intercom_client) { double('client') }
+    let(:intercom_events) { double('events') }
+
+    subject { IntercomService.send_published_content_event(content) }
+
+    before do
+      allow(Intercom::Client).to receive(:new).with(any_args).and_return(intercom_client)
+      allow(intercom_client).to receive(:events).and_return(intercom_events)
+    end
+
+    it 'should call `Intercom::Client#events.create`' do
+      expect(intercom_events).to receive(:create).with({
+        event_name: 'published-content',
+        email: content.created_by.email,
+        created_at: Time.current.to_i,
+        metadata: {
+          "organization_name": content.organization.name,
+          "number_of_published_posts": 0,
+          "post_title": content.title
+        }
+      })
+      subject
+    end
+  end
+end
