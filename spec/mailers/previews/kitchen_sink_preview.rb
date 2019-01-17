10eacf62b5ff8d1c026a0e3394abdec5daa6405c
Nick Gottlieb <ngottlieb@gmail.com>
2019-01-17 12:57:11 -0800

    [ch16392] remove kitchen sink mailer (#1682)

--- spec/mailers/previews/kitchen_sink_preview.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Preview all emails at http://localhost:3000/rails/mailers/kitchen_sink
-class KitchenSinkPreview < ActionMailer::Preview
-  def kitchen_sink
-    KitchenSinkMailer.show
-  end
-end
