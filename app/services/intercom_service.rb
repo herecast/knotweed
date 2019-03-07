abef073a9e295f7bfedb5d3af2fcbcf8694604be
Nick Gottlieb <ngottlieb@gmail.com>
2019-03-07 13:54:50 -0800

    [ch17066] update intercom service to create user if its not found (#1715)

--- app/services/intercom_service.rb
+++ app/services/intercom_service.rb
@@ -10,13 +10,30 @@ module IntercomService
 
 
     intercom = Intercom::Client.new(token: ENV['INTERCOM_ACCESS_TOKEN'])
+    begin
+      send_event(intercom, content, number_of_published_posts)
+    rescue Intercom::ResourceNotFound # user does not exist
+      create_user(intercom, content.created_by)
+      send_event(intercom, content, number_of_published_posts)
+    end
+  end
+
+  def create_user(intercom, user)
+    intercom.users.create(
+      email: user.email,
+      name: user.name,
+      signed_up_at: user.created_at
+    )
+  end
+
+  def send_event(intercom, content, num_posts)
     intercom.events.create(
       event_name: 'published-content',
       email: content.created_by.email,
       created_at: Time.current.to_i,
       metadata: {
         "organization_name": content.organization.name,
-        "number_of_published_posts": number_of_published_posts,
+        "number_of_published_posts": num_posts,
         "post_title": content.title
       }
     )
