fd14f4559145e1cbb88766fa35cea331569aea04
Nick Gottlieb <ngottlieb@gmail.com>
2019-06-24 13:13:19 -0700

    [ch17945] remove git version query and label from admin interface (#1769)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
   # normal Devise authentication
-  before_action :authorize_access!, :set_current_thread_user,
-                :get_version
+  before_action :authorize_access!, :set_current_thread_user
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: exception.message
@@ -21,11 +20,5 @@ class ApplicationController < ActionController::Base
 
   def set_current_thread_user
     User.current = current_user
-  end
-
-  def get_version
-    unless Rails.env.test?
-      @version ||= `git rev-parse --short HEAD`.chomp
-    end
   end
 end
