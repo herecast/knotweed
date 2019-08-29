c72a4efe208c19f3e40d9d5b5071f21403a11651
Nick Gottlieb <ngottlieb@gmail.com>
2019-08-29 10:46:18 -0700

    [ch18586] ensure user search (orgs, payment recipients) excludes archived users (#1808)

--- app/controllers/users/search_controller.rb
+++ app/controllers/users/search_controller.rb
@@ -2,6 +2,8 @@
 
 class Users::SearchController < ApplicationController
   def index
+    # always ignore archived users
+    params[:q][:archived_true] = false
     @users = User.ransack(params[:q])
       .result.page(1).per(10) # max 10 results, we're not actually paging
     # context is passed via params to define the action button in the view
