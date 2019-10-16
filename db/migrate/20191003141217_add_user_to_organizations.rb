fe7a3f774daf5d60d39adf05ac4cf47eb857f899
sheehan1102 <sheehan1102@gmail.com>
2019-10-16 12:46:35 -0400

    [ch18824] clean up old migrations (#1827)

--- db/migrate/20191003141217_add_user_to_organizations.rb
+++
@@ -1,6 +0,0 @@
-class AddUserToOrganizations < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :organizations, :user, foreign_key: true
-    User.find_each { |u| u.managed_organizations.first&.update_attribute(:user_id, u.id) }
-  end
-end
